30234ed674d83a13172594c2493a999ab4025fb2
AntonSerg <65146852+AntonSerg@users.noreply.github.com>
2020-05-11 07:43:57 +0200

    Add files via upload

---
+++ Haffman'sCodesWF/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Haffman_sCodesWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(197, 227);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "OpenFile";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(105, 227);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(260, 196);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Haffman'sCodesWF/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Haffman_sCodesWF
+{
+    public partial class Form1 : Form
+    {
+        string fileContent;
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string filePath = openFileDialog.FileName;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+                textBox1.Text = fileContent;
+            }
+        }
+        private void start()
+        {
+            textBox1.Invoke(new Action(() => { textBox1.Text = ""; }));
+
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+            char[] fileContentChar = fileContent.ToCharArray();
+            for (int i = 0; i < fileContentChar.Length; i++)
+            {
+                if (counter.ContainsKey(fileContentChar[i]))
+                {
+                    counter[fileContentChar[i]] += 1;
+                }
+                else
+                {
+                    counter.Add(fileContentChar[i], 1);
+                }
+            }
+            Dictionary<char, string> codes = new Dictionary<char, string>();
+            codes.Add(' ', intConvertToBite(1));
+            for (int i = 0; i < 26; i++)
+            {
+                codes.Add(Convert.ToChar('a' + i), intConvertToBite(i + 2));
+            }
+            string textBoxContent = "";
+            for (int i = 0; i < fileContentChar.Length; i++)
+            {
+                textBoxContent += codes[fileContentChar[i]];
+            }
+
+            textBox1.Invoke(new Action(() => { textBox1.Text = textBoxContent; }));
+
+        }
+        private string intConvertToBite(int n)
+        {
+            char[] array = new char[5] { '0', '0', '0', '0', '0' };
+            for (int i = 4; i >= 0; --i)
+            {
+                if (n % 2 == 0)
+                {
+                    array[i] = '0';
+                    n = n / 2;
+                }
+                else
+                {
+                    array[i] = '1';
+                    n = n / 2;
+                }
+                if (n == 0)
+                {
+                    return new string(array);
+                }
+            }
+            return new string(array);
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            th = new Thread(start);
+            th.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            th.Abort();
+        }
+    }
+}
